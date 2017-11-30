3177a96da6ec79aaf18c08f3a5801eaab462286f
vikaskasu92 <vikaskasu92@gmail.com>
2017-12-01 03:56:08 +0530

    Update librariumCheckout.js

--- public/scripts/librariumCheckout.js
+++ public/scripts/librariumCheckout.js
@@ -1,10 +1,32 @@
-var rentalArray = [];
-
 var dynamicClicks = function(){	
 	$(".trashButton").unbind().click(function(){
 		var currentId = $(this).parent().attr("id");
 		var currentIndex = currentId.substring(5, 7);
 		$("#tr"+currentIndex).remove();
+		$("#costOfBooks").text("$ "+calculateCostOfBooks());
+		var currentCostWithDollar = $("#costOfBooks").text();
+		var currentCostWithoutDollar = currentCostWithDollar.substring(2,5).trim();
+		if(parseInt(currentCostWithoutDollar) == 0){
+			window.location.href = "http://"+location.hostname+":"+location.port+"/";
+		}
+	});
+}
+
+var normalOnClick = function(){
+	var bookNameArray = [];
+	$("#rent").unbind().click(function(){
+		var rowCount =	$("#checkoutTbody").children().length;
+		for(var i=0; i<rowCount; i++){
+			var bookNameToReduce = $("#checkoutTbody").children().eq(i).children().eq(0).text();
+			bookNameArray[i] = bookNameToReduce;
+		}
+		updateAvailableBooks(bookNameArray,rowCount);
+		$("#confirmRent").modal({backdrop: 'static',keyboard: false});
+	});
+
+	$("#closeConfirm").unbind().click(function(){
+		$("#confirmRent").modal("hide");
+		window.location.href = "http://"+location.hostname+":"+location.port+"/";
 	});
 }
 
@@ -22,23 +44,6 @@ var openDB = function(){
 var getObjectStore = function(db,e){
 	db = e.target.result;
 	var objectStore = db.transaction(["ITMD462-562-Project-UserDB"], 'readwrite').objectStore("ITMD462-562-Project-UserDB");
-	return objectStore;
-}
-
-var openDB3 = function(){
-	var request = indexedDB.open("ITMD462-562-Project-UserDB", 3);
-	request.onupgradeneeded = function(e){
-		var thisDb = e.target.result;
-		if(! thisDb.objectStoreNames.contains("ITMD462-562-Project-OrdersDB")){
-			thisDb.createObjectStore("ITMD462-562-Project-OrdersDB",{autoIncrement:true});
-		}
-	}
-	return request;
-}
-
-var getObjectStore3 = function(db,e){
-	db = e.target.result;
-	var objectStore = db.transaction(["ITMD462-562-Project-OrdersDB"], 'readwrite').objectStore("ITMD462-562-Project-OrdersDB");
 	return objectStore;
 }
 
@@ -69,22 +74,27 @@ var openDBToCreateTable = function(){
 	   		 		}
    		 		}
    		 		dynamicClicks();
-   		 		calculateCostOfBooks();
    		 	}
    		}
    	}
 }
 
-var createDB2 = function(){
+var updateAvailableBooks = function(bookNameArray,rowCount){
 	var db;
-	var request = openDB3();
-	var currentThread = 0;
+	var request = openDB();
 	request.onsuccess = function(e) {
-		var objectStore = getObjectStore3(db,e);
+		var objectStore = getObjectStore(db,e);
 		objectStore.openCursor().onsuccess = function(event) {
    		 	var cursor = event.target.result;
    		 	if(cursor){
-   		 		cursor.continue();
+   		 		for(var j=0; j<rowCount; j++){
+   		 			for(var i=0; i<10; i++){
+		   		 		if(bookNameArray[j] === cursor.value.booksData[i].bookName){
+		   		 			cursor.value.booksData[i].availableBooks = parseInt(cursor.value.booksData[i].availableBooks) - 1;
+		   		 			cursor.update(cursor.value);
+		   		 		}
+   		 			}
+   		 		}
    		 	}
    		}
    	}
@@ -99,21 +109,48 @@ var setRentalDatesAndStrikeFields = function(){
 		objectStore.openCursor().onsuccess = function(event) {
    		 	var cursor = event.target.result;
    		 	if(cursor){
-   		 		rentalArray = [{"member":cursor.value.member,"rentalDate":cursor.value.date}];
    		 		$("#dateOfRental").text("Date of Rental :"+cursor.value.date);
+   		 		$("#memberTrueOrFlase").text(cursor.value.member);
+   		 		$("#costOfBooks").text("$ "+calculateCostOfBooks());
+   		 		var rowCount =	$("#checkoutTbody").children().length;
+   		 		if($("#memberTrueOrFlase").text() == "true"){
+		 			$("#categorizedTable").children().children().children().eq(3).addClass("hide");
+ 					for(var i=0; i<rowCount; i++){
+ 						var hidingNonMemberCost = $("#checkoutTbody").children().eq(i).children().eq(3).addClass("hide");
+ 					}
+   		 		}else{
+   		 			$("#categorizedTable").children().children().children().eq(4).addClass("hide");
+ 					for(var i=0; i<rowCount; i++){
+ 						var hidingMemberCost = $("#checkoutTbody").children().eq(i).children().eq(4).addClass("hide");
+ 					}
+   		 		}
    		 	}
    		}
    	}
 }
 
-
+var calculateCostOfBooks = function(){
+	 var countArray = [];
+	 var price = 0;
+	 var rowCount =	$("#checkoutTbody").children().length;
+	 for(var i=0; i<rowCount; i++){
+	 	if($("#memberTrueOrFlase").text() == "true"){
+	 		var originalCostWithDollar = $("#checkoutTbody").children().eq(i).children().eq(4).text();
+	 	}else{
+	 		var originalCostWithDollar = $("#checkoutTbody").children().eq(i).children().eq(3).text();
+	 	}
+	 	countArray[i] = originalCostWithDollar.substring(1,4).trim();
+	 }
+	 for(var i=0; i<countArray.length; i++){
+	 	price = price + parseInt(countArray[i]);
+	 }
+	 return price;
+}
 
 $(document).ready(function(){
+	normalOnClick();
 	openDBToCreateTable();
 	setTimeout(function(){ 
 		setRentalDatesAndStrikeFields();
-		setTimeout(function(){ 
-			createDB2();
-		}, 500);
-	}, 400);
+	}, 200);
 });
