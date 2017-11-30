d85796788fc031f707f99e74630a2ec89d1cdf52
vikaskasu92 <vikaskasu92@gmail.com>
2017-11-30 14:01:48 +0530

    Update librariumCheckout.js

--- public/scripts/librariumCheckout.js
+++ public/scripts/librariumCheckout.js
@@ -16,6 +16,23 @@ var getObjectStore = function(db,e){
 	return objectStore;
 }
 
+var openDB2 = function(){
+	var request = indexedDB.open("ITMD462-562-Project-UserDB", 2);
+	request.onupgradeneeded = function(e){
+		var thisDb = e.target.result;
+		if(! thisDb.objectStoreNames.contains("ITMD462-562-Project-OrdersDB")){
+			thisDb.createObjectStore("ITMD462-562-Project-OrdersDB",{autoIncrement:true});
+		}
+	}
+	return request;
+}
+
+var getObjectStore2 = function(db,e){
+	db = e.target.result;
+	var objectStore = db.transaction(["ITMD462-562-Project-OrdersDB"], 'readwrite').objectStore("ITMD462-562-Project-OrdersDB");
+	return objectStore;
+}
+
 var createTableRowsForCheckout = function(index,cursor){
 	var sNo = index+1;
 	var trOpen = $("<tr>");
@@ -49,8 +66,26 @@ var openDBToCreateTable = function(){
    	}
 }
 
+var createDB2 = function(){
+	var db;
+	var request = openDB2();
+	var currentThread = 0;
+	request.onsuccess = function(e) {
+		var objectStore = getObjectStore2(db,e);
+		objectStore.openCursor().onsuccess = function(event) {
+   		 	var cursor = event.target.result;
+   		 	if(cursor){
+   		 		cursor.continue();
+   		 	}
+   		}
+   	}
+}
+
 
 
 $(document).ready(function(){
 	openDBToCreateTable();
+	setTimeout(function(){ 
+		createDB2();
+	}, 800);
 });
