2df2c8f6bf61cab15860ae9b53b015d687dea9f8
Aury <abwashi@hawk.iit.edu>
2017-12-03 16:11:50 -0600

    Working on page that displays based on a given genre!

---
+++ public/scripts/category.js
@@ -0,0 +1,18 @@
+$(document).ready( function () {
+  $('#fiction').click(function(){
+    console.log('fiction picked!');
+  });
+
+  $('#nonfiction').click(function(){
+    console.log('non fiction picked!');
+  });
+
+  $('#biographies').click(function(){
+    console.log('biographies picked!');
+  });
+  
+  $('#thrillers').click(function(){
+    console.log('thrillers picked!');
+  });
+}
+);
